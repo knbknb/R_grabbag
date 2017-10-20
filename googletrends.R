99e220a844a912171400673c4eba7fa56caed7ce
Knut Behrends <knb@gfz-potsdam.de>
2017-10-20 10:32:26 +0200

    modify soccer player trends search

--- googletrends.R
+++ googletrends.R
@@ -3,6 +3,7 @@ if(! require("gtrendsR"))         install_github("knbknb/gtrendsR")
 set.seed(1)
 library(gtrendsR)
 library(ggplot2)
+library(dplyr)
 
 deeplearn_trend <- gtrends(c("theano", "tensorflow", "keras"),  geo = c("DE"), hl="DE")
 #deeplearn_trend.xts <- as.xts(x= deeplearn_trend$trend[,2:length(deeplearn_trend$trend)], index=deeplearn_trend$start)
@@ -25,19 +26,26 @@ ggplot(trend_reg, aes(keyword, hits)) +
         facet_wrap( ~location , ncol = 2) +
         ggtitle("Google trends: Deep Learning frameworks", subtitle = "Searches in Germany's 17 States, last 5 years")
 
+library(gtrendsR)
+library(ggplot2)
 
-energy_drinks_trend <- gtrends(c("red bull", "Bier", "Coca Cola"),  geo = c("DE"), hl="DE")
+energy_drinks_trend <- gtrends(c("red bull", "Bier", "Coca Cola", "Fanta"),  geo = c("DE"), hl="DE")
 energy_drinks_trend <- energy_drinks_trend$interest_over_time
 ggplot(energy_drinks_trend, aes(date, hits, color=keyword)) +
         geom_line() +
         ylab("Hits/Day") +
         xlab("Year") +
-        ggtitle("Google trends: Energy Drinks Trend", subtitle = "Searches in Germany, last 5 years")
+        ggtitle("Google trends: Soft Drinks", subtitle = "Searches in Germany, last 5 years")
 
 
-# soccer players with weird names
-#soccer_trend <- gtrends(c("Kolasinac", "Aubameyang"), res="7d", geo = c("DE"))
-#plot(soccer_trend)
+# soccer players with weird names #kolasinac
+soccer_trend <- gtrends(c("Neymar", "Aubameyang", "Lewandowski"), geo = c("DE"), hl="DE")
+soccer_trend <- soccer_trend$interest_over_time
+ggplot(soccer_trend, aes(date, hits, color=keyword)) +
+        geom_line() +
+        ylab("Hits/Day") +
+        xlab("Year") +
+        ggtitle("Google trends: Soccer Players", subtitle = "Searches in Germany, last 5 years")
 
 
 #library(dtw)
@@ -47,24 +55,3 @@ ggplot(energy_drinks_trend, aes(date, hits, color=keyword)) +
 #dbpedia_trend <- gtrends(c("dbpedia", "tensorflow"), res="7d")
 #plot(dbpedia_trend)
 
-
-
-####################################### DOES NOT WORK #########################################################
-###
-# cotton_trend <- gtrends(c("cotton", "satin", "velvet"), time = "now 7-d")
-# d <- dist(t(cotton_trend$trend[, -(1:2)])) #, method="DTW")
-# hc <- hclust(d)
-#
-#
-# par(mfrow=c(1,2))
-# plot(cotton~end, cotton_trend$trend, type="l", ylim=range(cotton_trend$trend[, -(1:2)]), col=3, ylim="")
-# for (x in 4:ncol(cotton_trend$trend))
-#         lines(x=cotton_trend$trend$end, y=cotton_trend$trend[, x], col=x)
-#
-# legend("topleft", names(cotton_trend$trend)[-(1:2)], lty=1, col=3:ncol(cotton_trend$trend))
-#
-# plot(hc)
-# rect.hclust(hc, k=2)
-#
-#
-# cutree(hc, k=2)
